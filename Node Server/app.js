0baf0af2a12b4f77dc26802a19bd4d0d7d412ca1
Diego Ramirez <diego_ramirezmillan@hotmail.com>
2020-07-17 09:46:57 -0600

    advance routing

--- Node Server/app.js
+++ Node Server/app.js
@@ -1,26 +1,24 @@
 const path = require('path');
-const http = require('http'); // If is a local folder use ./ or /
-const bodyParser = require('body-parser');
+
 const express = require('express');
-const expressHbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.engine('handlebars', expressHbs());
-app.set('view engine', 'handlebars');
-app.set('views', 'views')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404');
-});
+app.use(errorController.get404);
 
 app.listen(8080);
